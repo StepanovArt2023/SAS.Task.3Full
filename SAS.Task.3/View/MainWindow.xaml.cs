29a5f26c1e5cf0cb0c5fd586f09f0135ae5f9209
artem <artem@DESKTOP-7S1DBR4>
2023-02-27 15:25:19 +0300

    Добавьте файлы проекта.

---
+++ SAS.Task.3/View/MainWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿using SAS.Task._3.View.Pages;
+using SAS.Task._3.Core;
+using System.ComponentModel;
+using System.Windows;
+
+namespace SAS.Task._3
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            CoreNavigate.MyConnection = MainFrame;
+            MainFrame.Navigate(new MainPage());
+
+        }
+
+    }
+}
